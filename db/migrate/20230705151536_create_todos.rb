6a84f31a46c84cd109e49c5be81dde10f9dbaea4
ahmadi1401 <ahmadi1355esmaeel@gmail.com>
2023-07-17 10:55:13 +0430

    remove add_column_to_todo

--- db/migrate/20230705151536_create_todos.rb
+++ db/migrate/20230705151536_create_todos.rb
@@ -1,7 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
-
+      t.string :task
+      t.boolean :complete, default: false
       t.timestamps
     end
   end
--- db/migrate/20230705185729_add_column_to_todo.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToTodo < ActiveRecord::Migration[7.0]
-  def change
-    add_column :todos, :task, :string
-    add_column :todos, :complete, :boolean, default: false
-  end
-end
